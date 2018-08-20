007783eaa77181f2b1c32cca597f55707176579b
Alena Pahuda <Alena_Pahuda@epam.com>
2018-08-20 17:18:07 +0300

    add some sleep

--- test/spec3.js
+++ test/spec3.js
@@ -3,12 +3,14 @@
 describe ('some tests', () => {
 
     it(' should have sign in and create account buttons on the tart screen', () => {
+        browser.pause(10000);
         expect(browser.getText('#sign_in')).toEqual('Sign In');
         expect(browser.getText('#create_account')[0]).toEqual('Create Account');
         browser.click('#sign_in');
     });
 
     it(' should open log in page and logging in', () => {
+        browser.pause(10000);
         expect(browser.getText('#title')).toEqual('USER LOGIN');
         browser.setValue('#username', 'elenapogudo@protonmail.com');
         browser.setValue('#password', '123456789');
@@ -18,10 +20,12 @@ describe ('some tests', () => {
     });
 
     it(' should have one letter inbox', () => {
+        browser.pause(10000);
         expect(browser.getText('#message_title')).toEqual('Welcome to ProtonMail! Learn about your encrypted inbox');
     });
 
     it(' should open letter', () => {
+        browser.pause(10000);
       browser.click('#message_title');
         expect(browser.getText('#message_title')).toEqual('Welcome to ProtonMail! Learn about your encrypted inbox');
     });
